4ab30ba8a849e8e85b6dc09c104cb61028bcffca
aerodynamics <yusuke.takahashi@gmail.com>
2026-06-11 14:17:47 +0900

    Fix firebase-config.js again

--- firebase-config.js
+++ firebase-config.js
@@ -8,5 +8,5 @@ window.__FB = {
   authDomain: "tensenses-model.firebaseapp.com",
   projectId:  "tensenses-model",
   appId:      "1:1036666585144:web:1bac62d1cfc53a5a0826cd",        // e.g. "1:1234567890:web:abcdef..."
-  sheetEndpoint: "https://script.google.com/macros/s/AKfycbxguIY8PhzPwN2xagVCJtR3JWqTedrUYuOrZ1SqVN2oDjTbhFwHLkW0itchYAiyV7l9JA/exec"  // ← ②のURL（任意）
+  sheetEndpoint: "https://script.google.com/macros/s/AKfycbxU-ODJtb5iIzAV32T9F5DaAnvyt5Lq5RuvrcFCfxAU_NrkgQSuFxkPJQBVASWj6zgG4A/exec"  // ← ②のURL（任意）
 };
